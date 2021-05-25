ddb308bdcabcbecd16d64e62aeaa778971bdfae4
Anil Kaundal <anilkaundal1999@gmail.com>
2021-05-26 00:00:18 +0530

    Create connect four game

---
+++ connect_four_game.js
@@ -0,0 +1,7 @@
+/*
+Make a 2-player Connect Four game, where the board is 7 units wide,
+and 6 units high. Your program should take in user input when a player
+puts in a piece, and have functions boardState() (which returns a 2D
+array of the current state of the board) and hasWon() which returns
+which player has won the game (if any).
+*/
